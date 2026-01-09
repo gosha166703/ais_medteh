4cb7a808a90ee4c61fa0e2802f4471e5380c4b7c
Georgiy Ilyasov <gosha166703@icloud.com>
2026-01-09 19:29:21 +0300

    Изменены html, добавлена программа загрузки csv файла

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
@@ -6,9 +7,9 @@ from sqlalchemy import select
 
 from webapp.config import Config
 from webapp.db import db
-from webapp.forms import LoginForm
-from webapp.models_user import User, Role
-from webapp.models_fixed_assed import FixedAssed
+from webapp.forms import LoginForm, EquipmentForm
+from webapp.models_user import User
+from webapp.models_equipment import Equipment
 
 
 
@@ -32,11 +33,101 @@ def create_app():
     # Инициализируем миграции
     migrate = Migrate(app, db)
     
-    #Главная страница 
+    #Главная страница (Аналитика)
     @app.route('/')
     def index():
         return render_template('index.html', page_title="Главная")
     
+    #Эндпоинт Реестр основных средств
+    @app.route("/equipment")
+    def equipment():
+        #Получить все оборудование из БД 
+        stmt = select(Equipment).order_by(Equipment.created_at.desc())
+        equipment_list = db.session.execute(stmt).scalars().all()
+        return render_template("equipment.html", page_title="Реестр основных средств",
+                               equipment_list=equipment_list)
+    
+    #Эндпоинт Создание оборудования
+    @app.route("/equipment/create", methods=["GET","POST"])
+    @login_required
+    def create_equipment():
+        form = EquipmentForm()
+        if form.validate_on_submit():
+            new_equipment = Equipment(name=form.name.data,
+                                      purpose=form.purpose.data,
+                                      serial_number=form.serial_number.data,
+                                      inventory_number=form.inventory_number.data,
+                                      date_entry=form.date_entry.data,
+                                      status=form.status.data,
+                                      write_off=form.write_off.data,
+                                      model=form.model.data,
+                                      manufacturer=form.manufacturer.data,
+                                      #time_work=timedelta(hours=form.time_work.data or 0),
+                                      #downtime=timedelta(hours=form.downtime.data or 0),
+                                      last_maintenance=form.last_maintenance.data,
+                                      )
+            try:
+                db.session.add(new_equipment)
+                db.session.commit()
+                flash("Оборудование добавлено в реестр", 'success')
+                return redirect(url_for("equipment"))
+            except Exception as e:
+                db.session.rollback()
+                flash(f"Ошибка при добавлении: {str(e)}", "error") 
+
+        return render_template("equipment_form.html", form=form, page_title="Добавить оборудование")
+    
+    #Эндпоинт Просмотр оборудования
+    @app.route("/equipment/<int:id>", methods=["GET"])
+    @login_required
+    def equipment_detail(id):
+        equipment = db.session.get(Equipment, id)
+        if not equipment:
+            flash("Оборудование не найдено", "error")
+            return redirect(url_for("equipment"))
+        return render_template("equipment_detail.html", equipment=equipment, page_title=equipment.name)
+    
+    #Эндпоинт Редактирование оборудования
+    @app.route("/equipment/<int:id>/edit", methods=["GET", "POST"])
+    @login_required
+    def equipment_edit(id):
+        #Обработка GET запроса
+        equipment = db.session.get(Equipment, id)
+        if not equipment:
+            flash("Оборудование не найдено", "error")
+            return redirect(url_for("equipment"))
+        form = EquipmentForm(obj=equipment)
+        
+        #Обработка POST запроса
+        if form.validate_on_submit():
+            form.populate_obj(equipment)
+            try:
+                db.session.commit()
+                flash("Изменения сохранены", "success")
+                return redirect(url_for("equipment_detail", id=id))
+            except Exception as e:
+                db.session.rollback()
+                flash(f"Ошибка при изменении: {str(e)}", "error")
+        return render_template("equipment_form.html", form=form, 
+                          page_title="Редактировать оборудование", equipment=equipment)
+
+    
+    #Эндпоинт Удаление оборудования
+    @app.route("/equipment/<int:id>/delete", methods=["POST"])
+    @login_required
+    def delete_equipment(id):
+        equipment = db.session.get(Equipment, id)
+        try:
+            db.session.delete(equipment)
+            db.session.commit()
+            flash ("Оборудование удалено", 'success')
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Ошибка при удалении: {str(e)}', 'error')
+            
+        return redirect(url_for('equipment'))
+
+        
     #Cтраница авторизации. Аторизация, проверка есть ли такой пользователь в системе,
     #проверка авторизовывался ли уже пользователь
     @app.route('/login')
@@ -58,7 +149,7 @@ def create_app():
             user = db.session.execute(stmt).scalar_one_or_none()
 
             if user and user.check_password(form.password.data):
-                login_user(user)
+                login_user(user, remember=form.remember_me.data)
                 flash("Авторизация прошла успешно")
                 return redirect(url_for("index"))
             
--- webapp/forms.py
+++ webapp/forms.py
@@ -1,8 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import BooleanField, DateField, IntegerField, SelectField, StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional, ValidationError
 
+from datetime import datetime
+
+#Форма авторизации
 class LoginForm(FlaskForm):
     username = StringField("Имя пользователя", validators=[DataRequired()])
     password = PasswordField("Пароль", validators=[DataRequired()])
+    remember_me = BooleanField("Запомнить меня")
+
     submit = SubmitField("Отправить")
+
+#Форма создания и редактирования оборудования 
+class EquipmentForm(FlaskForm):
+    name = StringField("Название", validators= [DataRequired(message="Обязательное поле"),
+                                               Length(max=1000, message="Не более 1000 символов")])
+    purpose = StringField("Назначение", validators=[Optional()])
+    serial_number = StringField("Серийный номер", validators=[Optional()])
+    inventory_number = StringField("Инвентарный номер", validators=[Optional()])
+    date_entry = DateField("Дата ввода в эксплуатацию",format='%Y-%m-%d',
+                           validators=[Optional()])
+    status = SelectField("Статус", choices=[('active', 'Исправно'),
+                                            ('inactive', 'В резерве'),
+                                            ('repair', 'На ремонте'),
+                                            ('not_active', 'Не исправно')
+                                        ], validators=[DataRequired()])
+    write_off = BooleanField("Оборудование списано?", default=False)
+    model = StringField("Модель", validators=[Optional()])
+    manufacturer = StringField("Производитель", validators=[Optional()])
+    #time_work = IntegerField("Наработка(часы)", validators=[Optional()], default=0)
+    #downtime =  IntegerField("Время простоя(часы)", validators=[Optional()],default=0)
+    last_maintenance = DateField("Дата последнего ТО", format='%Y-%m-%d',
+                                 validators=[Optional()])
+
+    submit = SubmitField("Сохранить")
